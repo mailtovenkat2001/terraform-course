b2e43a0c8371803ac6ddb2eae198ad91cff8a219
mailtovenkat2001 <37097353+mailtovenkat2001@users.noreply.github.com>
2020-04-24 18:05:49 +0100

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,1 +1,8 @@
-aws s3api create-bucket --bucket my-bucket --region us-east-1
+#!/bin/bash
+sudo yum -y update
+
+echo "Install Java JDK 8"
+yum remove -y java
+yum install -y java-1.8.0-openjdk
+
+echo "successfully completed jenkins job sample "
