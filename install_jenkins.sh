d7fa6e3e5a828c93f1c4848961d2780a17978ede
mailtovenkat2001 <37097353+mailtovenkat2001@users.noreply.github.com>
2020-04-24 17:33:54 +0100

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,1 +1,10 @@
 echo "Successfully deployed"
+
+sudo cd /usr/local/src
+
+wget https://releases.hashicorp.com/terraform/0.8.5/terraform_0.8.5_linux_386.zip
+
+unzip terraform_0.8.5_linux_386.zip
+
+mv terraform /usr/local/bin/
+
