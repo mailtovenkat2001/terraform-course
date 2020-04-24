cdd8d21875b4b834ae643b4539c9311a66c8df94
mailtovenkat2001 <37097353+mailtovenkat2001@users.noreply.github.com>
2020-04-24 17:51:53 +0100

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,3 +1,19 @@
+##-----------------------------
+##  Terraform: Installation  ##
+##-----------------------------
+## Create an EC2 AWS Linux 2 instance and an IAM user with admin access
+## and programettic access (Get the Access key and Secret Key)
+## Connect to the AWS instance
+
+############################
+## Terraform installation ##
+############################
+## Get terraform package
+wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+
+## Unzip the package
+unzip terraform_0.11.13_linux_amd64.zip
+
 ## Move the package to /usr/local/bin
 sudo mv terraform /usr/local/bin/ && rm terraform_0.11.13_linux_amd64.zip
 
